b4276069e6e7c3ab81cccc9af1a3e7519c24d2e2
n <nopensource@protonmail.com>
2023-09-08 21:09:19 +0200

    Changed request to take id of the object instead of the full object

--- src/main/java/com/n/opensource/reddoit/requests/CreateUserRequest.java
+++ src/main/java/com/n/opensource/reddoit/requests/CreateUserRequest.java
@@ -4,6 +4,7 @@ import com.n.opensource.reddoit.model.entity.Recipe;
 import lombok.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @Getter
 @Setter
@@ -14,5 +15,5 @@ public class CreateUserRequest {
     public String username;
     public String email;
     public String password;
-    public List<Recipe> recipes;
+    public List<UUID> recipes;
 }
